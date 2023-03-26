198df89a56a0d856999eee649cc7e3733f1ed37c
Sayemhaque <mdsayemofficial1000@gmail.com>
2023-03-27 01:51:16 +0600

    getting product from child

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Product = (props) => {
-    console.log(props.product)
     const {name,img,price,quantity,ratings,ratingsCoun,seller,shipping,stock}  = props.product;
+   const handleCart = props.handleProductCart;
+   
     return (
         <div>
            <div className='bg-gray-200 relative  rounded-lg w-full h-full border-1 p-2 border-gray-300 '>
@@ -11,7 +12,7 @@ const Product = (props) => {
               <span className='text-sm font-bold mt-2'>{name}</span>
               <p className='text-sm font-bold mt-2'>Price: ${price}</p>
               <p className='text-sm font-bold mt-2 mb-3'>Manufaturer: ${seller}</p>
-              <button className='w-full absolute   bottom-0 py-2 left-0 rounded-lg text-white bg-slate-800'>Add to cart</button>
+              <button className='w-full absolute   bottom-0 py-2 left-0 rounded-lg text-white bg-slate-800' onClick={() => handleCart(props.product)}>Add to cart</button>
             </div>
            </div>
         </div>
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -3,14 +3,17 @@ import Product from './Product';
 
 const Products = () => {
     const [products , setProducts] = useState([]);
-
+    
+    const handleCart = (product) => {
+        console.log(product)
+    }
     useEffect(() => {
         const URL = 'products.json';
         fetch(URL).then(res => res.json()).then(data => setProducts(data))
     } , [])
     return (
         <div className='grid grid-cols-1 md:grid-cols-3 gap-8 m-10 '>
-            {products?.map(product =>  <Product key={product.id} product={product}/>)}
+            {products?.map(product =>  <Product key={product.id} handleProductCart={handleCart} product={product}/>)}
         </div>
     );
 };
