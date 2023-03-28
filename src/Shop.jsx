60e28b14555e38cf554a57c4380aa354b7474906
Sayemhaque <mdsayemofficial1000@gmail.com>
2023-03-28 17:39:50 +0600

    local storage problem solved

--- src/Shop.jsx
+++ src/Shop.jsx
@@ -8,7 +8,17 @@ const Shop = () => {
     const [cart , setCart] = useState([])
     
     const handleCart = (product) => {
-        const newCart = [...cart , product];
+        let newCart = [];
+       
+        const exist = cart.find(p => p.id === product.id)
+        if(!exist){
+            product.quantity = 1
+            newCart = [...cart , product];
+        }else{
+            exist.quantity = exist.quantity + 1;
+            const remaining = cart.filter(p => p.id !== product.id)
+            newCart = [...remaining ,exist]
+        }
         setCart(newCart)
         addToDb(product.id)
     }
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,13 +4,18 @@ import React from 'react';
 const Cart = ({ cartItems }) => {
 
     let total = 0;
+    let quantity = 0;
     for(const product of cartItems){
-     total = total + product.price;
+        product.quantity = product.quantity || 1;
+     total = total + product.price * product.quantity;
+     quantity = quantity + product.quantity;
+     
+
     }
     return (
         <div className='mt-8'>
             <h2 className='text-center font-bold text-lg'>Order summary</h2>
-            <h3 className='text-center font-bold mt-4'>Total items : {cartItems.length}</h3>
+            <h3 className='text-center font-bold mt-4'>Total items : {quantity}</h3>
             <h4 className='text-md font-bold mt-3 text-center'>Total price: ${total}</h4>
             {cartItems.length === 0 ? <p className='mt-5 text-center text-lg font-bold'>No cart items</p> :
                 cartItems.map(cart =>
